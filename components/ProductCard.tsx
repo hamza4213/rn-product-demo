3a09af681b5dab18a6daad684076f3c37317742a
hamza4213 <hamzashabbir4213@gmail.com>
2025-10-14 10:56:54 +0500

    products list suporting light and dark mode

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { Product } from "@/src/api/hooks/useProducts";
 import { useFavoritesStore } from "@/src/store/useFavoritesStore";
 import React from "react";
@@ -7,31 +8,55 @@ export const ProductCard = ({ item }: { item: Product }) => {
   const favorites = useFavoritesStore((state) => state.favorites);
   const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
   const favorite = favorites.includes(item.id);
+  const background = useThemeColor({}, "card");
+  const text = useThemeColor({}, "text");
+  const secondary = useThemeColor({}, "textSecondary");
+  const favoriteActive = useThemeColor({}, "favoriteActive");
+  const favoriteInactive = useThemeColor({}, "favoriteInactive");
   return (
-    <View style={styles.card}>
+    <View style={[styles.card, { backgroundColor: background }]}>
       <Image
         source={{ uri: item.image }}
         style={styles.image}
         resizeMode="contain"
       />
-      <Text style={styles.title} numberOfLines={1}>
+
+      <Text style={[styles.title, { color: text }]} numberOfLines={1}>
         {item.title}
       </Text>
-      <Text style={styles.category}>{item.category}</Text>
-      <Text style={styles.description} numberOfLines={2}>
+
+      <Text style={[styles.category, { color: secondary }]}>
+        {item.category}
+      </Text>
+
+      <Text
+        style={[styles.description, { color: secondary }]}
+        numberOfLines={2}
+      >
         {item.description}
       </Text>
+
       <View style={styles.row}>
-        <Text style={styles.rating}>
+        <Text style={[styles.rating, { color: secondary }]}>
           ⭐ {item.rating.rate} ({item.rating.count})
         </Text>
-        <Text style={styles.price}>${item.price.toFixed(2)}</Text>
+        <Text style={[styles.price, { color: text }]}>
+          ${item.price.toFixed(2)}
+        </Text>
       </View>
+
       <TouchableOpacity
-        style={[styles.favoriteButton, favorite && styles.favoriteActive]}
+        style={[
+          styles.favoriteButton,
+          {
+            backgroundColor: favorite ? favoriteActive : favoriteInactive,
+          },
+        ]}
         onPress={() => toggleFavorite(item.id)}
       >
-        <Text style={styles.favoriteText}>{favorite ? "♥" : "♡"}</Text>
+        <Text style={[styles.favoriteText, { color: text }]}>
+          {favorite ? "♥" : "♡"}
+        </Text>
       </TouchableOpacity>
     </View>
   );
