0fb59c46ef2560a22dbcd2046df9cc8257cf2c3e
hamza4213 <hamzashabbir4213@gmail.com>
2025-10-14 14:49:47 +0500

    colors assigned

--- app/(tabs)/(products)/product-list.tsx
+++ app/(tabs)/(products)/product-list.tsx
@@ -1,4 +1,5 @@
 import { ProductCard } from "@/components/ProductCard";
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { useProducts } from "@/src/api/hooks/useProducts";
 import { useFavoritesStore } from "@/src/store/useFavoritesStore";
 import { useEffect } from "react";
@@ -7,21 +8,22 @@ import { ActivityIndicator, FlatList, Text, View } from "react-native";
 export default function ProductListScreen() {
   const { data, isLoading, error } = useProducts();
   const loadFavorites = useFavoritesStore((state) => state.loadFavorites);
-
+  const backgroundColor = useThemeColor({}, "background");
+  const textColor = useThemeColor({}, "text");
   useEffect(() => {
     loadFavorites();
   }, []);
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#000" />
+        <ActivityIndicator size="large" color={backgroundColor} />
       </View>
     );
   }
   if (error) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Error loading products list.</Text>
+        <Text style={{ color: textColor }}>Error loading products list.</Text>
       </View>
     );
   }
--- app/(tabs)/(products)/productDetails.tsx
+++ app/(tabs)/(products)/productDetails.tsx
@@ -1,50 +1,101 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { useProductDetail } from "@/src/api/hooks/useProductDetails";
+import { useFavoritesStore } from "@/src/store/useFavoritesStore";
 import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import { useEffect } from "react";
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function ProductDetailScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { data: product, isLoading, error } = useProductDetail(id!);
+  const text = useThemeColor({}, "text");
+  const background = useThemeColor({}, "card");
+  const secondary = useThemeColor({}, "textSecondary");
+  const favoriteActive = useThemeColor({}, "favoriteActive");
+  const favoriteInactive = useThemeColor({}, "favoriteInactive");
+  const toggleFavorite = useFavoritesStore((state) => state.toggleFavorite);
+  const isFavorite = useFavoritesStore((state) => state.isFavorite);
+  const loadFavorites = useFavoritesStore((state) => state.loadFavorites);
+
+  useEffect(() => {
+    loadFavorites();
+  }, []);
 
   if (isLoading) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ActivityIndicator size="large" color="#000" />
+        <ActivityIndicator size="large" color={background} />
       </View>
     );
   }
-
-  if (error || !product) {
+  if (error) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Error loading product details.</Text>
+        <Text style={{ color: text }}>Error loading products list.</Text>
       </View>
     );
   }
 
   return (
     <ScrollView style={{ flex: 1, padding: 16 }}>
-      <Image
-        source={{ uri: product.image }}
+      <View style={{ position: "relative" }}>
+        <Image
+          source={{ uri: product?.image }}
+          style={{
+            width: "100%",
+            height: 300,
+            borderRadius: 8,
+            marginBottom: 16,
+          }}
+          resizeMode="contain"
+        />
+        <TouchableOpacity
+          onPress={() => toggleFavorite(product?.id!)}
+          style={{
+            position: "absolute",
+            top: 16,
+            right: 16,
+            padding: 8,
+            borderRadius: 24,
+            backgroundColor: "rgba(255,255,255,0.9)",
+          }}
+        >
+          <Text
+            style={{
+              color: isFavorite(product?.id!) ? "#ff6b6b" : "#333",
+              fontSize: 20,
+            }}
+          >
+            {isFavorite(product?.id!) ? "♥" : "♡"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text
         style={{
-          width: "100%",
-          height: 300,
-          borderRadius: 8,
-          marginBottom: 16,
+          fontSize: 24,
+          fontWeight: "bold",
+          marginBottom: 8,
+          color: text,
         }}
-        resizeMode="contain"
-      />
-      <Text style={{ fontSize: 24, fontWeight: "bold", marginBottom: 8 }}>
-        {product.title}
+      >
+        {product?.title}
       </Text>
       <Text style={{ fontSize: 18, color: "#2e7d32", marginBottom: 8 }}>
-        ${product.price.toFixed(2)}
+        ${product?.price.toFixed(2)}
       </Text>
-      <Text style={{ fontSize: 16, color: "#555", marginBottom: 12 }}>
-        {product.category}
+      <Text style={{ fontSize: 16, color: secondary, marginBottom: 12 }}>
+        {product?.category}
       </Text>
-      <Text style={{ fontSize: 16, lineHeight: 22 }}>
-        {product.description}
+      <Text style={{ fontSize: 16, lineHeight: 22, color: secondary }}>
+        {product?.description}
       </Text>
 
       <View
@@ -55,9 +106,9 @@ export default function ProductDetailScreen() {
           justifyContent: "space-between",
         }}
       >
-        <Text style={{ fontSize: 16 }}>⭐ {product.rating.rate} / 5</Text>
-        <Text style={{ fontSize: 14, color: "#777" }}>
-          ({product.rating.count} reviews)
+        <Text style={{ fontSize: 16 }}>⭐ {product?.rating.rate} / 5</Text>
+        <Text style={{ fontSize: 14, color: secondary }}>
+          ({product?.rating.count} reviews)
         </Text>
       </View>
     </ScrollView>
